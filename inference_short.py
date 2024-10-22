108fd77e58870a76efeccdf3e3701865cef0cd06
Kyryll Kochkin <kyrylo.kochkin@nure.ua>
2024-10-22 14:37:06 +0200

    update inference_short to print generated text

--- inference_short.py
+++ inference_short.py
@@ -2,7 +2,9 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig
 from model import *
 
 #model_path = "k050506koch/GPT3-dev"
-model_path = "gpt3-small-fineweb"
+model_path = "your_local_model_path"
 tokenizer = AutoTokenizer.from_pretrained(model_path)
 model = CustomGPT2LMHeadModel.from_pretrained(model_path)
-print(model)
+text = (model.generate(tokenizer.encode("Hello, my name is", return_tensors="pt")))
+text2 = tokenizer.decode(text[0], skip_special_tokens=False)
+print(text2)
